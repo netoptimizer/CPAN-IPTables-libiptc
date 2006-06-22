8f501f7aa9280485d7852327f0a00f7b77eb026d
Jesper Dangaard Brouer <hawk@comx.dk>
2006-06-22 12:07:27 +0000

    Creating new module IPTables::libiptc.
    This module will be a C interface with iptables library "libiptc".
    
    All these files are generated by the "h2xs" perl util.

---
+++ lib/IPTables/libiptc.pm
@@ -0,0 +1,117 @@
+package IPTables::libiptc;
+
+use 5.008004;
+use strict;
+use warnings;
+use Carp;
+
+require Exporter;
+use AutoLoader;
+
+our @ISA = qw(Exporter);
+
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
+# This allows declaration	use IPTables::libiptc ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+	IPT_MIN_ALIGN
+) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw(
+	IPT_MIN_ALIGN
+);
+
+our $VERSION = '0.01';
+
+sub AUTOLOAD {
+    # This AUTOLOAD is used to 'autoload' constants from the constant()
+    # XS function.
+
+    my $constname;
+    our $AUTOLOAD;
+    ($constname = $AUTOLOAD) =~ s/.*:://;
+    croak "&IPTables::libiptc::constant not defined" if $constname eq 'constant';
+    my ($error, $val) = constant($constname);
+    if ($error) { croak $error; }
+    {
+	no strict 'refs';
+	# Fixed between 5.005_53 and 5.005_61
+#XXX	if ($] >= 5.00561) {
+#XXX	    *$AUTOLOAD = sub () { $val };
+#XXX	}
+#XXX	else {
+	    *$AUTOLOAD = sub { $val };
+#XXX	}
+    }
+    goto &$AUTOLOAD;
+}
+
+require XSLoader;
+XSLoader::load('IPTables::libiptc', $VERSION);
+
+# Preloaded methods go here.
+
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+IPTables::libiptc - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use IPTables::libiptc;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for IPTables::libiptc, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+=head2 Exportable constants
+
+  IPT_MIN_ALIGN
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Jesper Dangaard Brouer, E<lt>jdb@slackware.lanE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by Jesper Dangaard Brouer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ libiptc.xs
@@ -0,0 +1,13 @@
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+#include "ppport.h"
+
+#include <include/libiptc/libiptc.h>
+
+#include "const-c.inc"
+
+MODULE = IPTables::libiptc		PACKAGE = IPTables::libiptc		
+
+INCLUDE: const-xs.inc
