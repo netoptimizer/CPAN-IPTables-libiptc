efd377e540f8dfee8cb59758fa6bed27aa0a4223
Jesper Dangaard Brouer <hawk@comx.dk>
2008-06-13 14:12:32 +0000

    Lets try these changes on some system...

--- lib/IPTables/libiptc.pm
+++ lib/IPTables/libiptc.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 	IPT_MIN_ALIGN
 );
 
-our $VERSION = '0.06-rc1';
+our $VERSION = '0.06-rc2';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
