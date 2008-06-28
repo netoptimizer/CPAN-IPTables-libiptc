a600b73902499f900d217b3aae4af53e090ffb6e
Jesper Dangaard Brouer <hawk@comx.dk>
2008-06-28 10:17:28 +0000

    Bump the version to a test version 0.10-test01.

--- lib/IPTables/libiptc.pm
+++ lib/IPTables/libiptc.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 	IPT_MIN_ALIGN
 );
 
-our $VERSION = '0.09';
+our $VERSION = '0.10-test01';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
