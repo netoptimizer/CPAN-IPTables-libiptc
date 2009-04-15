3eb7fe330935bc4b34ec18c7cdb43f96682f6ad5
Jesper Dangaard Brouer <hawk@comx.dk>
2009-04-15 08:24:15 +0000

    Add a patches directory, to keep track of incomming patches.

--- lib/IPTables/libiptc.pm
+++ lib/IPTables/libiptc.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 	IPT_MIN_ALIGN
 );
 
-our $VERSION = '0.12';
+our $VERSION = '0.13-rc1';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
