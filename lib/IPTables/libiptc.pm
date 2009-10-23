d3e767319cf0669366e56ae0bda0192cc2930fa9
Jesper Dangaard Brouer <hawk@comx.dk>
2009-10-23 15:26:31 +0000

    Bump version to 0.18-dev01 of IPTables::libiptc. This is a development ver that might not compile, will try to support several version of iptables.

--- lib/IPTables/libiptc.pm
+++ lib/IPTables/libiptc.pm
@@ -42,7 +42,7 @@ our @EXPORT = qw(
 	IPT_MIN_ALIGN
 );
 
-our $VERSION = '0.17';
+our $VERSION = '0.18-dev01';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
