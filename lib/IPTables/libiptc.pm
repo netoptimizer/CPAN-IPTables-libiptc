1a895bf18b8cbd7ef0d9cbe825b02b9aa01182d1
Jesper Dangaard Brouer <hawk@comx.dk>
2011-06-29 10:05:35 -0400

    GPL license, correct the license text
    This module is obviously under GPL as it includes the iptables.c
    files from the Netfilter project, which is under GPL.  Previously
    it stated that this package as under a Perl license, which was false.
    
    Signed-off-by: Jesper Dangaard Brouer <hawk@comx.dk>

--- lib/IPTables/libiptc.pm
+++ lib/IPTables/libiptc.pm
@@ -275,8 +275,9 @@ Jesper Dangaard Brouer, E<lt>hawk@diku.dkE<gt> or E<lt>hawk@people.netfilter.org
 
 Copyright (C) 2006-2009 by Jesper Dangaard Brouer
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.4 or,
-at your option, any later version of Perl 5 you may have available.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 =cut
